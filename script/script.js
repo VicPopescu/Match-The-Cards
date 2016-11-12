cefa40f5d65ebc4564418107a54a8bd740eb910c
Jenci <kudorjenci@yahoo.com>
2016-11-13 01:01:49 +0200

    username handling

--- script/script.js
+++ script/script.js
@@ -1,31 +1,52 @@
-var loginSection = $(".login");
-var gameSection = $(".game");
-var cardContainer = $(".cardContainer");
-var numberOfCards = 0;
+// jQuery selectors
+var $loginSection = $(".login");
+var $gameSection = $(".game");
+var $cardContainer = $(".cardContainer");
+var $userName = $(".userName");
+var $loginButton = $(".startButton");
+var $difficultySelect = $(".difficulty");
+var $userTitle = $(".userTitle");
 
-var loginButton = $(".startButton");
+// variables
+var numberOfCards = 0;
+var difficulty = '';
 
 //append cards
-var howManyCards = function(cardNumber, style){
+var howManyCards = function(cardNumber, style) {
     var cardStyle = "";
+    var card = "";
+
     if (style == "easyStyle") cardStyle = "cardEasy";
     if (style == "mediumStyle") cardStyle = "cardMedium";
     if (style == "hardStyle") cardStyle = "cardHard";
 
-  var card = "<div class='" + cardStyle + "'></div>";
+    card = "<div class='" + cardStyle + "'></div>";
 
-  for (var i = 0; i < cardNumber; i++){
-    cardContainer.append(card);
-  }
+    for (var i = 0; i < cardNumber; i++) {
+        $cardContainer.append(card);
+    }
 
 }
 
-loginButton.on("click", function(){
-    loginSection.css("display", "none");
-    gameSection.css({
+// on start
+$loginButton.on("click", function(){
+
+    // initialising userName
+    var userName = "";
+    userName = $userName.val();
+    if (userName == ""){
+      userName = "Player1";
+    };
+    console.log(userName);
+    $userTitle.text(userName);
+
+
+    $loginSection.css("display", "none");
+    $gameSection.css({
         "display":"block",
         "background-color":"red"
     });
+
     switch (difficulty) {
       case 'easy': howManyCards(16, "easyStyle")
       break;
@@ -37,16 +58,11 @@ loginButton.on("click", function(){
     }
 })
 
-
-
 // handling difficulty
-var difficultySelect = $(".difficulty");
-var difficulty = '';
-
-difficulty = difficultySelect.val();
+difficulty = $difficultySelect.val();
 console.log(difficulty);
 
-difficultySelect.change(function(){
-  difficulty = difficultySelect.val();
+$difficultySelect.change(function(){
+  difficulty = $difficultySelect.val();
   console.log(difficulty);
 });
